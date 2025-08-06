1499aefe44d38f47ec41982c336e14faef802381
hardik5838 <hderashri@asepeyo.es>
2025-08-06 14:20:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ import plotly.express as px
 #######################
 # Page configuration
 st.set_page_config(
-    page_title="US Population Dashboard",
-    page_icon="🏂",
+    page_title="Energy Efficiency Dashboard",
+    page_icon="⚡",
     layout="wide",
     initial_sidebar_state="expanded")
 
